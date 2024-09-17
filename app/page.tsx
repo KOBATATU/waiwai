1c9f36b9019419ac7fcdc9c8ed8e2ade6cf8df6a
KOBATATU <kobatatu258@yahoo.co.jp>
2024-09-17 20:21:52 +0900

    feature: lp

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-"use client"
-
-import { signIn } from "next-auth/react"
+import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
+import { SiteHeader } from "@/components/Header/Header"
 
 export default function IndexPage() {
   return (
     <div>
-      <Button onClick={() => signIn("github")}>login</Button>
+      <SiteHeader />
+      <div className="flex items-center justify-center h-[80vh] ">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold  mb-4">😀waiwai😀</h1>
+          <p className="text-xl text-gray-700 mb-4">
+            Let's enjoy the company competition
+          </p>
+          <Link href="/competitions">
+            <Button variant="outline">start</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
