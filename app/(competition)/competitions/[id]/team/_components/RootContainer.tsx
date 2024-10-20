06265268acb4d4e90c5461b394d6bcb8a622e469
KOBATATU <kobatatu258@yahoo.co.jp>
2024-10-20 13:31:27 +0900

    feature: team member basic layout

--- app/(competition)/competitions/[id]/team/_components/RootContainer.tsx
+++ app/(competition)/competitions/[id]/team/_components/RootContainer.tsx
@@ -14,6 +14,17 @@ export const RootContainer = async ({ id }: RootContainerProps) => {
         <h2 className="text-xl font-bold ">Your Team</h2>
       </div>
       <TeamName competitionId={id} teamName={team.name} />
+
+      <div className="mt-4">
+        <h2 className="text-xl font-bold ">Team Member</h2>
+        {team.teamMembers.map((teamMember) => {
+          return (
+            <div key={teamMember.id} className="flex gap-2">
+              {teamMember.user.name}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
